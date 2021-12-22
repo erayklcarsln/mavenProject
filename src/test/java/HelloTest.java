2131108c67b81d12cd0c766f1619961a8290ad7f
EryKlcarsln <eryklcarsln@gmail.com>
2021-12-22 12:28:05 +0300

    hello.maven project

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,7 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("developer");
+        System.out.println("github");
+    }
+}
